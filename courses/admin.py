4c3e4a3e3d9077018241a2ed820e392edadc9697
Gilson Gindre <gindrejunior@hotmail.com>
2021-09-21 18:05:58 -0300

    Models Admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Rating
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'created', 'modified', 'active')
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('course', 'name', 'email', 'rating', 'created', 'modified', 'active')
