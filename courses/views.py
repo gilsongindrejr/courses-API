511ceea9633034ac804e1dd7926d91965c475cdb
Gilson Gindre <gindrejunior@hotmail.com>
2021-09-22 10:33:56 -0300

    Post method

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Course, Rating
 from .serializers import CourseSerializer, RatingSerializers
@@ -7,19 +8,31 @@ from .serializers import CourseSerializer, RatingSerializers
 
 class CourseAPIView(APIView):
     """
-    API for courses
+    Course API
     """
     def get(self, request):
         courses = Course.objects.all()
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class RatingAPIView(APIView):
     """
-    API for rating
+    Rating API
     """
     def get(self, request):
         ratings = Rating.objects.all()
         serializer = RatingSerializers(ratings, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
