376afb4f63b1b7156f57a157159a9f0cc95cf4cb
Gilson Gindre <gindrejunior@hotmail.com>
2021-09-22 12:10:23 -0300

    Override views methods

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,10 @@ from .views import CoursesAPIView, RatingsAPIView, CourseAPIView, RatingAPIView
 
 urlpatterns = [
     path('courses/', CoursesAPIView.as_view(), name='courses'),
-    path('ratings/', RatingsAPIView.as_view(), name='ratings'),
     path('courses/<int:pk>/', CourseAPIView.as_view(), name='course'),
-    path('ratings/<int:pk>/', RatingAPIView.as_view(), name='rating'),
+    path('courses/<int:course_pk>/ratings/', RatingsAPIView.as_view(), name='course_ratings'),
+    path('courses/<int:course_pk>/ratings/<int:rating_pk>/', RatingAPIView.as_view(), name='course_rating'),
+
+    path('ratings/', RatingsAPIView.as_view(), name='ratings'),
+    path('ratings/<int:rating_pk>/', RatingAPIView.as_view(), name='rating'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Course, Rating
 from .serializers import CourseSerializer, RatingSerializers
@@ -18,7 +19,21 @@ class RatingsAPIView(generics.ListCreateAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializers
 
+    def get_queryset(self):
+        if self.kwargs.get('course_pk'):
+            return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+        return self.queryset.all()
+
 
 class RatingAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializers
+
+    def get_object(self):
+        if self.kwargs.get('course_pk'):
+            return get_object_or_404(
+                self.get_queryset(),
+                course_id=self.kwargs.get('course_pk'),
+                pk=self.kwargs.get('rating_pk')
+            )
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('rating_pk'))
